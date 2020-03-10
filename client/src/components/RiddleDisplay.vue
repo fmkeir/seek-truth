98d17cdd452125f7c1d28b8f036a4660685ef97c
cowannoel <cowannoel@gmail.com>
2020-03-10 17:15:43 +0000

    riddle logic half way

--- client/src/components/RiddleDisplay.vue
+++ client/src/components/RiddleDisplay.vue
@@ -1,16 +1,32 @@
 <template lang="html">
   <div class="riddle">
     <h2> {{selectedShindig.riddleQuestion}}</h2>
-    <form>
-      <input type="text" name="" value="">
+    <form @submit.prevent="handleSubmit">
+      <input type="text" name="" v-model="answer">
     </form>
   </div>
 </template>
 
 <script>
+import { eventBus } from '@/main.js'
 export default {
   name: 'riddle',
-  props: ['selectedShindig']
+  props: ['selectedShindig'],
+  data() {
+    return {
+      answer: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      console.log('the answer is ', this.answer);
+      console.log('this answer is: ', this.selectedShindig.riddleAnswer === this.answer);
+
+      if (this.selectedShindig.riddleAnswer === this.answer){
+        eventBus.$emit('please-show-user-page', this.selectedShindig)
+      }
+    }
+  }
 }
 </script>
 
