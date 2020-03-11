25c2b6b6612c9958671a11ad13d9010b1f485b02
FKeir <fmkeir@gmail.com>
2020-03-11 21:27:55 +0000

    Style user page

--- client/src/components/UserPage.vue
+++ client/src/components/UserPage.vue
@@ -1,29 +1,67 @@
 <template lang="html">
   <div id="user-page">
-    <!-- <user-details :user="user"/> -->
-    <shindig-details :shindig="shindig"/>
+    <div class="page-container">
+      <header>
+        <h1>Welcome to the revelation NAME.</h1>
+        <h2>{{shindig.name}}, Shindig {{shindig.shindigNumber}}</h2>
+        <h2>Instructions: <br/><br/> {{shindig.instructions}}</h2>
+      </header>
+      <display-map
+        :latitude="shindig.locationLat"
+        :longitude="shindig.locationLong"
+        />
     </div>
+  </div>
 </template>
 
 <script>
-import UserDetails from '@/components/UserDetails.vue'
-import ShindigDetails from '@/components/ShindigDetails.vue'
+import DisplayMap from '@/components/DisplayMap.vue'
 
 export default {
   name: 'user-page',
   components: {
-    'user-details': UserDetails,
-    'shindig-details': ShindigDetails
+    'display-map': DisplayMap
   },
   props: ['user', 'shindig']
-
 }
 </script>
 
 <style lang="css" scoped>
-#user-page {
-  display: flex;
-  align-items: center;
+header {
   justify-content: center;
+  text-align: center;
+  padding: 2em;
+}
+#user-page {
+  width: 100vw;
+  height: 100vh;
+  padding-top: 2em;
+  background: #202020;
+  background: -webkit-linear-gradient(to bottom, #434343, #202020);
+  background: linear-gradient(to bottom, #434343, #202020);
+  background-size: 100% 300%;
+}
+
+.page-container {
+  display: grid;
+  grid-template-rows: 1fr 5fr;
+  width: auto;
+  max-width: 70%;
+  height: 90%;
+  line-height: 20px;
+  margin: auto;
+  background-color: white;
+  border-radius: 5px;
+  font-family: 'Nanum Gothic Coding', monospace;
+}
+
+h1 {
+  padding-bottom: 1em;
+  font-size: 24px;
+  justify-self: center;
+}
+
+display-map {
+  border: 5px solid white;
 }
 </style>
