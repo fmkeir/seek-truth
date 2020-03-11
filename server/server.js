edceca1e3229d3376a9bfe440a7d86fc4c88427b
cowannoel <cowannoel@gmail.com>
2020-03-11 10:21:01 +0000

    created custom riddle router

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const createRouter = require('./helpers/create_router.js');
+const errorLog = require('./helpers/error_log.js')
 
 const app = express();
 app.use(cors());
@@ -17,8 +18,22 @@ MongoClient.connect('mongodb://localhost:27017')
     const usersRouter = createRouter(usersCollection);
     const shindigsRouter = createRouter(shindigsCollection);
 
+    const riddlesRouter = express.Router();
+    riddlesRouter.get('/', (req, res) =>{
+      shindigsCollection.find().toArray()
+      .then(docs => docs.map(doc => {
+        return {
+          _id: doc._id,
+          riddleQuestion: doc.riddleQuestion
+        }
+      }))
+      .then(riddles => res.json(riddles))
+      .catch(errorLog)
+    })
+
     app.use('/api/users', usersRouter);
     app.use('/api/shindigs', shindigsRouter);
+    app.use('/api/riddles', riddlesRouter);
   })
   .catch(err => console.error(err));
 
