b7da6d2199803fbd61f52db5ce5316ddba37ad80
FKeir <fmkeir@gmail.com>
2020-05-19 23:43:22 +0100

    Can seed mongodb from docker-compose

---
+++ mongo-seed/start.sh
@@ -0,0 +1,4 @@
+mongo db:27017/shindig-server --eval "db.dropDatabase()"
+for collection in $(ls seeds | cut -d'.' -f1)
+do mongoimport --host db:27017 --db shindig-server --collection ${collection} --file /seeds/${collection}.json --jsonArray
+done
